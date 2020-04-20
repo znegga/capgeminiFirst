97f9eb38c84577cdfeac1dd702ae93240dec3161
Negga <zinash.negga@capgemini.com>
2020-04-20 11:21:57 -0400

    create new branch

---
+++ accountRegistration/accountRegistration/src/main/java/com/capgemini/accountregistration/AccountRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.accountregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountRegistrationApplication.class, args);
+	}
+
+}
